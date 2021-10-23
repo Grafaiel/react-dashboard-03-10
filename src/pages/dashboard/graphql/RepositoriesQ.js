06824a76be37c80cb9f5aa01483309e1aceed7e8
Gabriel Bercelline <gabrielbercelline@gmail.com>
2021-10-22 22:23:46 -0300

    RepositoriesQ gql

--- src/pages/dashboard/graphql/RepositoriesQ.js
+++ src/pages/dashboard/graphql/RepositoriesQ.js
@@ -1,25 +1,25 @@
 import { gql } from '@apollo/client';
 
 const RepositoriesQ = gql`
-  query Repositories($repositoryName: String!) {
-    repositories: $repositoryName) {
-      id
-      repositories(first: 10) {
-        totalCount
-        pageInfo {
-          hasNextPage
-          endCursor
-        }
-        nodes {
-          id
-          name
-          stargazers_count
-          forks_count
-          language
-        }
+  query Repositories($username: String!) {
+  repositoryOwner (login: $username) {
+    id
+    repositories(first: 10) {
+      totalCount
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        id
+        name
+        stargazerCount
+        forkCount
+        primaryLanguage { name }
       }
     }
   }
+}
 `;
 
 export default RepositoriesQ;
