15c4f342e98b9213ec90a1fb86962ae657752c44
Gabriel Bercelline <gabrielbercelline@gmail.com>
2021-10-25 13:55:30 -0300

    List com GraphQL

--- src/components/Repository/List/List.js
+++ src/components/Repository/List/List.js
@@ -1,46 +1,65 @@
-import SimpleBar from 'simplebar-react';
-import './List.css';
+import { useEffect, useState } from 'react';
+import SimpleBar from "simplebar-react";
+// import useApi from "../../../useApi";
+import RepositoryCard from "../Card/Card";
+import "./List.css";
 
 
-export default function RepositoryList({ title, children, loading }) {
+export default function RepositoryList({ username }) {
+  const [error, setError] = useState();
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("github_token");
+    fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: {
+        authorization: `bearer ${token}`,
+      },
+      body: JSON.stringify({
+        query: `
+        query Repositories {
+          repositoryOwner (login: "${username}") {
+            id
+            repositories(first: 10) {
+              totalCount
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+              nodes {
+                id
+                name
+                stargazerCount
+                forkCount
+                primaryLanguage { name }
+              }
+            }
+          }
+        }`,
+        variables: {},
+      })
+    }).then((r) => r.json())
+      .then((data) => {
+        setRepositories(data.data.repositoryOwner.repositories.nodes);
+      })
+  },[username])
+
   return (
     <div className="RepositoryList">
       <h3>
-        {title}
-        {loading && <span>Loading...</span>}
+        {/* Repositories
+        {loading && <span>Loading...</span>} */}
       </h3>
       <SimpleBar style={{ maxHeight: 500 }}>
-        <ul className="RepositoryList__content">
-          {children}
-        </ul>
+        <div className="RepositoryList__content">
+
+          {repositories?.map((repository) => (
+            <RepositoryCard key={repository.full_name} repo={repository} />
+          ))}
+
+        </div>
       </SimpleBar>
     </div>
-  )
-  // const { data, error, loading } = useApi({
-  //   url: `https://api.github.com/users/${username}/repos?per_page=10&page=1`
-  // });
-  // return (
-  //   <div className="RepositoryList">
-  //     <h3>
-  //       Repositories
-  //       {loading && <span>Loading...</span>}
-  //     </h3>
-  //     <SimpleBar style={{ maxHeight: 500 }}>
-  //       <div className="RepositoryList__content">
-  //         {error ? (
-  //           <div>
-  //             Algo de errado, não está certo!
-  //           </div>
-  //         ) : (
-  //           data?.map((repository) => (
-  //             <RepositoryCard
-  //               repo={repository}
-  //               key={repository.full_name}
-  //             />
-  //           ))
-  //         )}
-  //       </div>
-  //     </SimpleBar>
-  //   </div>
-  // )
+  );
 }
